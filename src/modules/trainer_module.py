7284778e3aec3885a9971408f97a09f6e8812420
m_bobrin <maksim.bobrin@skoltech.ru>
2023-04-08 00:11:21 +0300

    added training script with tracking

--- src/modules/trainer_module.py
+++ src/modules/trainer_module.py
@@ -1,11 +1,18 @@
 import typing as tp
+import os
+import json
+from copy import copy
+from typing import Tuple, Callable, Iterator, Optional, Dict
+import collections
+import time
+from tqdm.auto import tqdm
 
 # JAX/Flax/Optax
 import jax
 import jax.numpy as jnp
-import optax
 from flax import linen as nn
 from flax.training import train_state, checkpoints
+import optax
 
 # Loggers
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -61,9 +68,404 @@ class TrainerModule:
         self.model = self.model_class(**self.model_hparams)
         self.print_model(dummy_input)
         
+        # Set up functions
+        self.init_logger(logger_params)
+        self.create_jitted_functions()
+        self.init_model(dummy_input)
+        
     def print_model(self, dummy_input: tp.Any):
         print(self.model.tabulate(jax.random.PRNGKey(0), *dummy_input, train=True))
         
+    def init_logger(self, 
+                    logger_params: tp.Optional[dict] = None):
+        if logger_params is None:
+            logger_params = dict()
+        logging_dir = logger_params.get("log_dir", None)
+        if not logging_dir:
+            # if no path to logdir provided -> log into checkpoints dir
+            base_log_dir = logger_params.get("base_log_dir", "checkpoints/")
+            log_dir = os.path.join(base_log_dir, self.config["model_class"])
+            if 'logger_name' in logger_params:
+                log_dir = os.path.join(log_dir, logger_params['logger_name'])
+            version = None
+        else:
+            version = ''
+        logger_type = logger_params.get('logger_type', 'TensorBoard').lower() #default to TB
+        if logger_type == 'tensorboard':
+            self.logger = TensorBoardLogger(save_dir=log_dir,
+                                            version=version,
+                                            name=logger_params.get('project_name', None))
+        elif logger_type == 'wandb':
+            self.logger = WandbLogger(name=logger_params.get('project_name', None),
+                                      save_dir=log_dir,
+                                      version=version,
+                                      config=self.config)
+        # Save hyperparameters
+        log_dir = self.logger.log_dir
+        if not os.path.isfile(os.path.join(log_dir, 'hparams.json')):
+            os.makedirs(os.path.join(log_dir, 'metrics/'), exist_ok=True)
+            with open(os.path.join(log_dir, 'hparams.json'), 'w') as f:
+                json.dump(self.config, f, indent=4)
+        self.log_dir = log_dir
+        
+    def create_jitted_functions(self):
+        '''
+        Create jitted version of train/eval functions.
+        '''
+        train_step, eval_step = self.create_functions()
+        if self.debug:
+            print("No jitting in debug mode")
+            self.train_step = train_step
+            self.eval_step = eval_step
+        else:
+            self.train_step = jax.jit(train_step)
+            self.eval_step = jax.jit(eval_step)
+    
+    def init_model(self, dummy: tp.Any):
+        model_rng = jax.random.PRNGKey(self.seed)
+        model_rng, init_rng = jax.random.split(model_rng)
+        dummy_input = [dummy_input] if not isinstance(dummy_input, (list, tuple)) else dummy_input
+        variables = self.run_model_init(dummy_input, init_rng)
+        self.state = TrainState(step=0,
+                                apply_fn=self.model.apply,
+                                params=variables['params'],
+                                batch_stats=variables.get('batch_stats'),
+                                rng=model_rng,
+                                tx=None,
+                                opt_state=None)
+        
+    def run_model_init(self, dummy_input: tp.Any,
+                       init_rng: tp.Any) -> tp.Dict:
+        return self.model.init(init_rng, *dummy_input, train=True)
 
+    def init_optimizer(self,
+                       num_epochs : int,
+                       num_steps_per_epoch : int):
+        """
+        Initializes the optimizer and learning rate scheduler.
 
+        Args:
+          num_epochs: Number of epochs the model will be trained for.
+          num_steps_per_epoch: Number of training steps per epoch.
+        """
+        hparams = copy(self.optimizer_hparams)
 
+        # Initialize optimizer
+        optimizer_name = hparams.pop('optimizer', 'adamw')
+        if optimizer_name.lower() == 'adam':
+            opt_class = optax.adam
+        elif optimizer_name.lower() == 'adamw':
+            opt_class = optax.adamw
+        elif optimizer_name.lower() == 'sgd':
+            opt_class = optax.sgd
+        else:
+            assert False, f'Unknown optimizer "{opt_class}"'
+            
+        lr = hparams.pop('lr', 3e-4)
+        warmup = hparams.pop('warmup', 0)
+        lr_schedule = optax.warmup_cosine_decay_schedule(
+            init_value=0.0,
+            peak_value=lr,
+            warmup_steps=warmup,
+            decay_steps=int(num_epochs * num_steps_per_epoch),
+            end_value=0.01 * lr
+        )
+        # Clip gradients at max value, and evt. apply weight decay
+        transf = [optax.clip_by_global_norm(hparams.pop('gradient_clip', 1.0))]
+        if opt_class == optax.sgd and 'weight_decay' in hparams:  # wd is integrated in adamw
+            transf.append(optax.add_decayed_weights(hparams.pop('weight_decay', 0.0)))
+        optimizer = optax.chain(
+            *transf,
+            opt_class(lr_schedule, **hparams)
+        )
+        self.state = TrainState.create(apply_fn=self.state.apply_fn,
+                                       params=self.state.params,
+                                       batch_stats=self.state.batch_stats,
+                                       tx=optimizer,
+                                       rng=self.state.rng)
+        
+    def create_functions(self) -> Tuple[Callable[[TrainState, tp.Any], Tuple[TrainState, tp.Dict]],
+                                        Callable[[TrainState, tp.Any], Tuple[TrainState, tp.Dict]]]:
+        """
+        Creates and returns functions for the training and evaluation step. The
+        functions take as input the training state and a batch from the train/
+        val/test loader. Both functions are expected to return a dictionary of
+        logging metrics, and the training function a new train state. This
+        function needs to be overwritten by a subclass. The train_step and
+        eval_step functions here are examples for the signature of the functions.
+        """
+        def train_step(state : TrainState,
+                       batch : tp.Any):
+            metrics = {}
+            return state, metrics
+        def eval_step(state : TrainState,
+                      batch : tp.Any):
+            metrics = {}
+            return metrics
+        raise NotImplementedError
+    
+    def train_model(self,
+                    train_loader : Iterator,
+                    val_loader : Iterator,
+                    test_loader : Optional[Iterator] = None,
+                    num_epochs : int = 500) -> Dict[str, tp.Any]:
+        """
+        Starts a training loop for the given number of epochs.
+
+        Args:
+          train_loader: Data loader of the training set.
+          val_loader: Data loader of the validation set.
+          test_loader: If given, best model will be evaluated on the test set.
+          num_epochs: Number of epochs for which to train the model.
+
+        Returns:
+          A dictionary of the train, validation and evt. test metrics for the
+          best model on the validation set.
+        """
+        # Create optimizer and the scheduler for the given number of epochs
+        self.init_optimizer(num_epochs, len(train_loader))
+        # Prepare training loop
+        self.on_training_start()
+        best_eval_metrics = None
+        for epoch_idx in self.tracker(range(1, num_epochs+1), desc='Epochs'):
+            train_metrics = self.train_epoch(train_loader)
+            self.logger.log_metrics(train_metrics, step=epoch_idx)
+            self.on_training_epoch_end(epoch_idx)
+            # Validation every N epochs
+            if epoch_idx % self.check_val_every_n_epoch == 0:
+                eval_metrics = self.eval_model(val_loader, log_prefix='val/')
+                self.on_validation_epoch_end(epoch_idx, eval_metrics, val_loader)
+                self.logger.log_metrics(eval_metrics, step=epoch_idx)
+                self.save_metrics(f'eval_epoch_{str(epoch_idx).zfill(3)}', eval_metrics)
+                # Save best model
+                if self.is_new_model_better(eval_metrics, best_eval_metrics):
+                    best_eval_metrics = eval_metrics
+                    best_eval_metrics.update(train_metrics)
+                    self.save_model(step=epoch_idx)
+                    self.save_metrics('best_eval', eval_metrics)
+        # Test best model if possible
+        if test_loader is not None:
+            self.load_model()
+            test_metrics = self.eval_model(test_loader, log_prefix='test/')
+            self.logger.log_metrics(test_metrics, step=epoch_idx)
+            self.save_metrics('test', test_metrics)
+            best_eval_metrics.update(test_metrics)
+        # Close logger
+        self.logger.finalize('success')
+        return best_eval_metrics
+    
+    def train_epoch(self,
+                    train_loader : Iterator) -> Dict[str, tp.Any]:
+        """
+        Trains a model for one epoch.
+
+        Args:
+          train_loader: Data loader of the training set.
+
+        Returns:
+          A dictionary of the average training metrics over all batches
+          for logging.
+        """
+        # Train model for one epoch, and log avg loss and accuracy
+        metrics = collections.defaultdict(float)
+        num_train_steps = len(train_loader)
+        start_time = time.time()
+        for batch in self.tracker(train_loader, desc='Training', leave=False):
+            self.state, step_metrics = self.train_step(self.state, batch)
+            for key in step_metrics:
+                metrics['train/' + key] += step_metrics[key] / num_train_steps
+        metrics = {key: metrics[key].item() for key in metrics}
+        metrics['epoch_time'] = time.time() - start_time
+        return metrics
+
+    def eval_model(self,
+                   data_loader : Iterator,
+                   log_prefix : Optional[str] = '') -> Dict[str, tp.Any]:
+        """
+        Evaluates the model on a dataset.
+
+        Args:
+          data_loader: Data loader of the dataset to evaluate on.
+          log_prefix: Prefix to add to all metrics (e.g. 'val/' or 'test/')
+
+        Returns:
+          A dictionary of the evaluation metrics, averaged over data points
+          in the dataset.
+        """
+        # Test model on all images of a data loader and return avg loss
+        metrics = collections.defaultdict(float)
+        num_elements = 0
+        for batch in data_loader:
+            step_metrics = self.eval_step(self.state, batch)
+            batch_size = batch[0].shape[0] if isinstance(batch, (list, tuple)) else batch.shape[0]
+            for key in step_metrics:
+                metrics[key] += step_metrics[key] * batch_size
+            num_elements += batch_size
+        metrics = {(log_prefix + key): (metrics[key] / num_elements).item() for key in metrics}
+        return metrics
+
+    def is_new_model_better(self,
+                            new_metrics : Dict[str, tp.Any],
+                            old_metrics : Dict[str, tp.Any]) -> bool:
+        """
+        Compares two sets of evaluation metrics to decide whether the
+        new model is better than the previous ones or not.
+
+        Args:
+          new_metrics: A dictionary of the evaluation metrics of the new model.
+          old_metrics: A dictionary of the evaluation metrics of the previously
+            best model, i.e. the one to compare to.
+
+        Returns:
+          True if the new model is better than the old one, and False otherwise.
+        """
+        if old_metrics is None:
+            return True
+        for key, is_larger in [('val/val_metric', False), ('val/acc', True), ('val/loss', False)]:
+            if key in new_metrics:
+                if is_larger:
+                    return new_metrics[key] > old_metrics[key]
+                else:
+                    return new_metrics[key] < old_metrics[key]
+        assert False, f'No known metrics to log on: {new_metrics}'
+        
+    def tracker(self,
+                iterator : Iterator,
+                **kwargs) -> Iterator:
+        """
+        Wraps an iterator in a progress bar tracker (tqdm) if the progress bar
+        is enabled.
+
+        Args:
+          iterator: Iterator to wrap in tqdm.
+          kwargs: Additional arguments to tqdm.
+
+        Returns:
+          Wrapped iterator if progress bar is enabled, otherwise same iterator
+          as input.
+        """
+        if self.enable_progress_bar:
+            return tqdm(iterator, **kwargs)
+        else:
+            return iterator
+
+    def save_metrics(self,
+                     filename : str,
+                     metrics : Dict[str, tp.Any]):
+        """
+        Saves a dictionary of metrics to file. Can be used as a textual
+        representation of the validation performance for checking in the terminal.
+
+        Args:
+          filename: Name of the metrics file without folders and postfix.
+          metrics: A dictionary of metrics to save in the file.
+        """
+        with open(os.path.join(self.log_dir, f'metrics/{filename}.json'), 'w') as f:
+            json.dump(metrics, f, indent=4)
+
+    def on_training_start(self):
+        """
+        Method called before training is started. Can be used for additional
+        initialization operations etc.
+        """
+        pass
+
+    def on_training_epoch_end(self,
+                              epoch_idx : int):
+        """
+        Method called at the end of each training epoch. Can be used for additional
+        logging or similar.
+
+        Args:
+          epoch_idx: Index of the training epoch that has finished.
+        """
+        pass
+
+    def on_validation_epoch_end(self,
+                                epoch_idx : int,
+                                eval_metrics : Dict[str, tp.Any],
+                                val_loader : Iterator):
+        """
+        Method called at the end of each validation epoch. Can be used for additional
+        logging and evaluation.
+
+        Args:
+          epoch_idx: Index of the training epoch at which validation was performed.
+          eval_metrics: A dictionary of the validation metrics. New metrics added to
+            this dictionary will be logged as well.
+          val_loader: Data loader of the validation set, to support additional
+            evaluation.
+        """
+        pass
+
+    def save_model(self,
+                   step : int = 0):
+        """
+        Saves current training state at certain training iteration. Only the model
+        parameters and batch statistics are saved to reduce memory footprint. To
+        support the training to be continued from a checkpoint, this method can be
+        extended to include the optimizer state as well.
+
+        Args:
+          step: Index of the step to save the model at, e.g. epoch.
+        """
+        checkpoints.save_checkpoint(ckpt_dir=self.log_dir,
+                                    target={'params': self.state.params,
+                                            'batch_stats': self.state.batch_stats},
+                                    step=step,
+                                    overwrite=True)
+
+    def load_model(self):
+        """
+        Loads model parameters and batch statistics from the logging directory.
+        """
+        state_dict = checkpoints.restore_checkpoint(ckpt_dir=self.log_dir, target=None)
+        self.state = TrainState.create(apply_fn=self.model.apply,
+                                       params=state_dict['params'],
+                                       batch_stats=state_dict['batch_stats'],
+                                       # Optimizer will be overwritten when training starts
+                                       tx=self.state.tx if self.state.tx else optax.sgd(0.1),
+                                       rng=self.state.rng
+                                      )
+
+    def bind_model(self):
+        """
+        Returns a model with parameters bound to it. Enables an easier inference
+        access.
+
+        Returns:
+          The model with parameters and evt. batch statistics bound to it.
+        """
+        params = {'params': self.state.params}
+        if self.state.batch_stats:
+            params['batch_stats'] = self.state.batch_stats
+        return self.model.bind(params)
+
+    @classmethod
+    def load_from_checkpoint(cls,
+                             checkpoint : str,
+                             exmp_input : tp.Any) -> tp.Any:
+        """
+        Creates a Trainer object with same hyperparameters and loaded model from
+        a checkpoint directory.
+
+        Args:
+          checkpoint: Folder in which the checkpoint and hyperparameter file is stored.
+          exmp_input: An input to the model for shape inference.
+
+        Returns:
+          A Trainer object with model loaded from the checkpoint folder.
+        """
+        hparams_file = os.path.join(checkpoint, 'hparams.json')
+        assert os.path.isfile(hparams_file), 'Could not find hparams file'
+        with open(hparams_file, 'r') as f:
+            hparams = json.load(f)
+        hparams.pop('model_class')
+        hparams.update(hparams.pop('model_hparams'))
+        if not hparams['logger_params']:
+            hparams['logger_params'] = dict()
+        hparams['logger_params']['log_dir'] = checkpoint
+        trainer = cls(exmp_input=exmp_input,
+                      **hparams)
+        trainer.load_model()
+        return trainer
